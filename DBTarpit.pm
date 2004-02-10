c924245ba3fa3952bd3cd926dc5fccabe82985aa
Michael Robinton <michael@bizsystems.com>
2004-02-10 01:31:17 +0000

    Import of MIKER/IPTables-IPv4-DBTarpit-0.24 from CPAN.
    gitpan-cpan-distribution: IPTables-IPv4-DBTarpit
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         MIKER/IPTables-IPv4-DBTarpit-0.24.tar.gz
    gitpan-cpan-author:       MIKER
    gitpan-cpan-maturity:     released

--- DBTarpit.pm
+++ DBTarpit.pm
@@ -1,7 +1,7 @@
 # /*    all lines except VERSION are C comments
 package IPTables::IPv4::DBTarpit;
 use vars qw($VERSION);
-$VERSION = do { q| */ char version[] = "dbtarpit 0.23, 12-6-03"; /* | =~ /(\d+)\.(\d+)/; sprintf("%d.%02d",$1,$2)};
+$VERSION = do { q| */ char version[] = "dbtarpit 0.24, 2-9-04"; /* | =~ /(\d+)\.(\d+)/; sprintf("%d.%02d",$1,$2)};
 # returns $VERSION which is non-zero
 __END__
 
@@ -487,7 +487,7 @@ Michael Robinton <michael@bizsystems.com>
 
 =head1 COPYRIGHT AND LICENCE
 
-  Copyright 2003, Michael Robinton <michael@bizsystems.com>
+  Copyright 2003, 2004, Michael Robinton <michael@bizsystems.com>
  
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
